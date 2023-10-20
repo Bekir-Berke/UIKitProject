91ab27e740d29a122b8951715d7c40e66da4f05b
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-10-20 20:41:35 +0300

    Initial Commit

---
+++ UIKitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitProject
+//
+//  Created by Bekir Berke Yılmaz on 20.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
